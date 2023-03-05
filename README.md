a9585e1e22daa993db912b2c86ff8ba85b9d2428
BachuChewangPelden <bachu.cpelden2016@academy.bt>
2023-03-05 15:07:24 +0700

    button

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ToDoList
+For self improvement
---
+++ main.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('form');
+const input = form.querySelector('input');
+const ul = document.querySelector('.todo-list');
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+	const text = input.value.trim();
+	if (text !== '') {
+		const li = document.createElement('li');
+		const checkbox = document.createElement('input');
+		checkbox.type = 'checkbox';
+		const span = document.createElement('span');
+		span.textContent = text;
+		const deleteBtn = document.createElement('button');
+		deleteBtn.textContent = 'X';
+		deleteBtn.className = 'delete-btn';
+		li.appendChild(checkbox);
+		li.appendChild(span);
+		li.appendChild(deleteBtn);
+		ul.appendChild(li);
+		input.value = '';
+	}
+});
+
+ul.addEventListener('click', function(event) {
+	if (event.target.type === 'checkbox') {
+		event.target.parentNode.classList.toggle('completed');
+	} else if (event.target.classList.contains('delete-btn')) {
+		event.target.parentNode.remove();
+	}
+});
